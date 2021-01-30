dcfc708e1965ab20d3469bc12619c01f57ff6fc5
godot <godotc03@gmail.com>
2021-01-30 13:45:34 +0800

    Add L14 assets :

---
+++ L14 Shader Graph/OffsetController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffsetController : MonoBehaviour
+{
+    public float OffsetAmount;
+    MeshRenderer meshRender;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRender = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OffsetAmount = Mathf.Lerp(OffsetAmount, 0, Time.deltaTime);
+        meshRender.material.SetFloat("_Amount", OffsetAmount);
+        if (Input.GetButtonDown("Jump"))
+        {
+            OffsetAmount += 1.0f;
+        }
+    }
+}
